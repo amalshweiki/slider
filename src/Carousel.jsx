672af03b988847ebf535332f931c2f12565281fc
amalshweiki <amalshweiki88@gmail.com>
2023-09-14 14:23:18 +0300

    final commit

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -4,10 +4,28 @@ import { FaQuoteRight } from 'react-icons/fa';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 const Carousel = () => {
     const [people,setPeople]=useState(list)
+    const [currentPerson,setCurrentPerson]= useState(0)
  const prevSlide=()=>{
-    
+  setCurrentPerson((oldPerson)=>{
+    const result = (oldPerson - 1+ people.length) % people.length;
+    return result;
+  })
  };
- const nextSlide=()=>{};
+ const nextSlide=()=>{
+  setCurrentPerson((oldPerson)=>{
+    const result = (oldPerson + 1) % people.length;
+    return result;
+  })
+  
+};
+useEffect(() =>{
+  let sliderID = setInterval(() =>{
+    nextSlide();
+  }, 2000);
+  return () => {
+    clearInterval(sliderID);
+  };
+}, [currentPerson]);
 
   return (
     <section className="slider-container">
@@ -15,9 +33,11 @@ const Carousel = () => {
     people.map((person,personIndex)=>{
         const {id,image,name,title,quote}=person
         return (
-        <article className='slider'  style={{
-            transform: `translateX(${100 * (personIndex )})`,
-           
+        <article className='slide'  style={{
+            transform: `translateX(${100 * (personIndex - currentPerson)}%)`,
+           opacity: personIndex === currentPerson ? 1 :0,
+           visibility: personIndex === currentPerson ? 1 :0,
+
           }} key={id}>
             <img src={image} alt={name} className="person-img "/>
        <h5 className='name'>{name}</h5>
